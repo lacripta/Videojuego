682c1dc96ad3b5a1e7c5d483de5629dfb762f101
Jose Bocanegra <joseybv@gmail.com>
2019-05-13 18:46:36 -0500

    logica de batalla primer nivel

--- juego_v4/scenes/level2/level2.js
+++ juego_v4/scenes/level2/level2.js
@@ -8,8 +8,9 @@ MiJuego.level2.prototype = {
     juego.centerY = juego.height / 2;
 
     juego.load.audio("firebolt_sfx", [
-      "scenes/level2/assets/fireball-cast-1.ogg"
+      "scenes/level2/assets/sfx_fireball-cast-1.ogg"
     ]);
+    juego.load.audio("heal_sfx", ["scenes/level2/assets/sfx_heal_cast.ogg"]);
     juego.load.spritesheet(
       "mage",
       "scenes/level2/assets/idle_sprite.png",
@@ -22,6 +23,12 @@ MiJuego.level2.prototype = {
       475,
       470
     );
+    juego.load.spritesheet(
+      "enemy_mage",
+      "scenes/level2/assets/enemy_mage_sprite.png",
+      408,
+      424
+    );
 
     juego.load.image("fire_bolt", "scenes/level2/assets/fire_bolt.png");
     juego.load.image("bg", "scenes/level2/assets/mapa_2.png");
@@ -31,27 +38,177 @@ MiJuego.level2.prototype = {
   create() {
     juego.world.setBounds();
     juego.background = juego.add.image(0, 0, "bg");
+
+    juego.enemyBatallion = {
+      id: "main_force",
+      support: {},
+      troops: [],
+      troopHit: function(target) {
+        let that = this;
+        if (target.statistics.health <= 4) {
+          that.support.statistics.spells.prepareHeal.cast(target);
+          setTimeout(function() {
+            that.support.statistics.spells.prepareHeal.animation.stop();
+            that.support.statistics.spells.heal.cast(target);
+            target.motion.heal.play();
+          }, 3000);
+        }
+      },
+      troopDefeated: function(target) {
+        console.log(target.statistics.id + " has died.");
+      }
+    };
+
+    this.addLevelKey();
+    juego.mage = this.addMainCharacter();
+
+    let enemyMage = this.addEnemyWizard();
+    juego.enemyBatallion.support = enemyMage;
+
+    for (let row = 0; row < 3; row++) {
+      let offsetY = row * 40;
+
+      for (let col = 0; col < 2; col++) {
+        let offsetX = col * 80;
+        let id = "kn" + col + row;
+
+        let knight = this.addEnemyKnight(id, offsetX, offsetY);
+
+        if (!juego.key.statistics.owner) {
+          juego.key.statistics.owner = knight;
+          knight.frame = 5;
+          knight.statistics.pouch.slot1 = juego.key;
+        }
+
+        juego.enemyBatallion.troops.push(knight);
+      }
+    }
+  },
+  addEnemyWizard() {
+    let enemy_mage = juego.add.sprite(600, 500, "enemy_mage");
+    enemy_mage.scale.setTo(0.15, 0.15);
+    enemy_mage.scale.x *= -1;
+
+    let healAnim = enemy_mage.animations.add("cast_heal", [1, 2, 0], 8, false);
+    let prepareHealAnim = enemy_mage.animations.add(
+      "prepare_heal",
+      [1, 0],
+      12,
+      true
+    );
+    let healSfx = juego.add.audio("heal_sfx");
+
+    enemy_mage.statistics = {
+      id: "enemy_mage_support",
+      health: 10,
+      power: 2,
+      pouch: {
+        slot1: undefined,
+        slot2: undefined,
+        slot3: undefined,
+        slot4: undefined
+      },
+      defense: {
+        blunt: 1,
+        pierce: 1,
+        magic: 4
+      },
+      spells: {
+        heal: {
+          name: "Heal",
+          power: 3,
+          animation: healAnim,
+          sfx: healSfx,
+          casting: false,
+          cast: function(target) {
+            this.animation.play();
+            this.sfx.play();
+            console.log(target);
+            target.statistics.health += this.power;
+          }
+        },
+        prepareHeal: {
+          name: "Prepare Heal",
+          power: 3,
+          animation: prepareHealAnim,
+          casting: false,
+          cast: function(target) {
+            if (this.casting) {
+              this.animation.play();
+              this.casting = true;
+            }
+          }
+        }
+      }
+    };
+    return enemy_mage;
+  },
+  addEnemyKnight(id, offsetX, offsetY) {
+    let knight = juego.add.sprite(
+      juego.centerX + 40 + offsetX,
+      juego.centerY - -150 + offsetY,
+      "knight"
+    );
+
+    knight.statistics = {
+      id: id,
+      health: 10,
+      power: 2,
+      pouch: {
+        slot1: undefined,
+        slot2: undefined,
+        slot3: undefined,
+        slot4: undefined
+      },
+      defense: {
+        blunt: 8,
+        pierce: 4,
+        magic: 0
+      }
+    };
+
+    knight.scale.setTo(0.2, 0.2);
+    knight.scale.x *= -1;
+    knight.inputEnabled = true;
+
+    let hurt = knight.animations.add("hurt", [1, 2, 0], 8, false);
+    let heal = knight.animations.add("heal", [3, 4, 0], 8, false);
+    let die = knight.animations.add("die", [6, 7, 8], 8, false);
+
+    knight.motion = {
+      hurt: hurt,
+      heal: heal,
+      die: die
+    };
+
+    knight.events.onInputDown.add(this.enemyKinghtHitCallback, this);
+    return knight;
+  },
+  addLevelKey() {
     juego.key = juego.add.image(0, 0, "key");
     juego.key.visible = false;
     juego.key.statistics = {
       owner: undefined
     };
-    juego.firebolt_sfx = juego.add.audio("firebolt_sfx");
+  },
+  addMainCharacter() {
+    let mage = juego.add.sprite(100, 500, "mage");
+    mage.scale.setTo(0.15, 0.15);
+    let fireboltAnim = mage.animations.add(
+      "cast_fire_bolt",
+      [1, 2, 0],
+      8,
+      false
+    );
+    let fireboltSfx = juego.add.audio("firebolt_sfx");
 
-    juego.mage = juego.add.sprite(100, 500, "mage");
-    juego.mage.scale.setTo(0.15, 0.15);
-    juego.mage.animations.add("cast_fire_bolt", [1, 2, 0], 8, false);
-
-    juego.mage.animations.currentAnim.onComplete.add(function(
-      target,
-      animation
-    ) {
-      if (juego.key.statistics.owner.statistics.id === target.statistics.id) {
-        target.frame = 5;
+    mage.animations.currentAnim.onComplete.add(function(_self) {
+      if (juego.key.statistics.owner.statistics.id === _self.statistics.id) {
+        _self.frame = 5;
       }
     });
 
-    juego.mage.statistics = {
+    mage.statistics = {
       id: "mage",
       health: 800,
       power: 12,
@@ -69,8 +226,14 @@ MiJuego.level2.prototype = {
       spells: {
         firebolt: {
           name: "Firebolt",
-          animation: "cast_fire_bolt",
-          power: 1
+          animation: fireboltAnim,
+          sfx: fireboltSfx,
+          power: 1,
+          cast: function(target) {
+            this.animation.play();
+            this.sfx.play();
+            target.statistics.health -= this.power;
+          }
         },
         nova: {
           name: "Nova",
@@ -84,95 +247,35 @@ MiJuego.level2.prototype = {
         }
       }
     };
-
-    for (let row = 0; row < 3; row++) {
-      let offsetY = row * 40;
-
-      for (let col = 0; col < 2; col++) {
-        let offsetX = col * 80;
-        let id = "kn" + col + row;
-        let knight = juego.add.sprite(
-          juego.centerX + 40 + offsetX,
-          juego.centerY - -150 + offsetY,
-          "knight"
-        );
-
-        if (!juego.key.statistics.owner) {
-          juego.key.statistics.owner = knight;
-          knight.frame = 5;
-        }
-
-        knight.statistics = {
-          id: id,
-          health: 10,
-          power: 2,
-          pouch: {
-            slot1: juego.key
-          },
-          defense: {
-            blunt: 8,
-            pierce: 4,
-            magic: 0
-          }
-        };
-
-        knight.scale.setTo(0.2, 0.2);
-        knight.scale.x *= -1;
-        knight.inputEnabled = true;
-
-        let hurt = knight.animations.add("hurt", [1, 2, 0], 8, false);
-        let heal = knight.animations.add("heal", [3, 4, 0], 8, false);
-        let die = knight.animations.add("die", [6, 7, 8], 8, false);
-
-        knight.motion = {
-          hurt: hurt,
-          heal: heal,
-          die: die
-        };
-
-        knight.events.onInputDown.add(function(sprite, pointer) {
-          let knight = juego.knight[sprite.statistics.id];
-          juego.mage.play("cast_fire_bolt");
-          juego.firebolt_sfx.play();
-
-          sprite.statistics.health -=
-            juego.mage.statistics.spells.firebolt.power;
-
-          if (sprite.statistics.health <= 0) {
-            sprite.play("die");
-
-            if (
-              juego.key.statistics.owner.statistics.id === sprite.statistics.id
-            ) {
-              juego.mage.statistics.pouch.slot1 = juego.key;
-              juego.key.statistics.owner = juego.mage;
-              juego.mage.frame = 5;
-            }
-
-            setTimeout(function() {
-              sprite.destroy();
-            }, 1000);
-          } else {
-            sprite.frame = 0;
-            sprite.play("hurt");
-          }
-
-          sprite.animations.currentAnim.onComplete.add(function(
-            target,
-            animation
-          ) {
-            if (
-              juego.key.statistics.owner.statistics.id === target.statistics.id
-            ) {
-              target.frame = 5;
-            }
-          });
-        }, this);
-
-        juego.knight[id] = knight;
-      }
-    }
+    return mage;
   },
+  enemyKinghtHitCallback(sprite, pointer) {
+    juego.mage.statistics.spells.firebolt.cast(sprite);
+    juego.enemyBatallion.troopHit(sprite);
 
+    if (sprite.statistics.health <= 0) {
+      sprite.motion.die.play();
+
+      if (juego.key.statistics.owner.statistics.id === sprite.statistics.id) {
+        juego.mage.statistics.pouch.slot1 = juego.key;
+        juego.key.statistics.owner = juego.mage;
+        juego.mage.frame = 5;
+      }
+
+      setTimeout(function() {
+        sprite.destroy();
+        juego.enemyBatallion.troopHit(sprite);
+      }, 1000);
+    } else {
+      sprite.frame = 0;
+      sprite.motion.hurt.play();
+    }
+
+    sprite.animations.currentAnim.onComplete.add(function(_self, animation) {
+      if (juego.key.statistics.owner.statistics.id === _self.statistics.id) {
+        _self.frame = 5;
+      }
+    });
+  },
   update() {}
 };
